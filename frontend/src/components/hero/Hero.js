16fdc84f6601f5f6cb016960ea2c864553816555
iamlydial <lydia.lavecchia@gmail.com>
2024-08-26 09:18:51 +0200

    add hero style and global css

--- frontend/src/components/hero/Hero.js
+++ frontend/src/components/hero/Hero.js
@@ -5,19 +5,46 @@ import { Paper } from "@mui/material";
 const Hero = ({ movies }) => {
   return (
     <div className="movie-carousel-container">
-      <h1>Hero</h1>
       <Carousel>
         {movies && movies.length > 0 ? (
           movies.map((movie, index) => {
             return (
-              <Paper key={index}>
+              <Paper key={movie.imdbId}>
                 <div className="movie-card-container">
-                  <div className="movie-card">
-                    <div className="movie-poster">
-                      <img src={movie.poster} alt={movie.title} />
-                    </div>
-                    <div className="movie-title">
-                      <h4>{movie.title}</h4>
+                  <div
+                    className="movie-card"
+                    style={{ "--img": `url(${movie.backdrops[0]})` }}
+                  >
+                    <div className="movie-detail">
+                      <div className="movie-poster">
+                        <img src={movie.poster} alt="" />
+                      </div>
+                      <div className="movie-title">
+                        <h4>{movie.title}</h4>
+                      </div>
+                      <div className="movie-buttons-container">
+                        {/* <Link
+                          to={`/Trailer/${movie.trailerLink.substring(
+                            movie.trailerLink.length - 11
+                          )}`}
+                        >
+                          <div className="play-button-icon-container">
+                            <FontAwesomeIcon
+                              className="play-button-icon"
+                              icon={faCirclePlay}
+                            />
+                          </div>
+                        </Link> */}
+
+                        {/* <div className="movie-review-button-container">
+                          <Button
+                            variant="info"
+                            onClick={() => reviews(movie.imdbId)}
+                          >
+                            Reviews
+                          </Button>
+                        </div> */}
+                      </div>
                     </div>
                   </div>
                 </div>
